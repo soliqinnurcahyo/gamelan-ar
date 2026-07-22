6f383f100747a6dac9115e6b6a91867b7d71aeb2
CEMPELAKNAT <soliqin01@gmail.com>
2026-07-23 01:25:18 +0700

    Update gamelan tidak muncl

--- src/ar/ModelManager.js
+++ src/ar/ModelManager.js
@@ -1,7 +1,12 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
 
 const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
 
 export function loadModel(data) {
 
